24ac5483b798cc1997ab05f21eb61b4996061a3d
NaseatDev <naseatman2017@gmail.com>
2024-08-05 15:21:04 +0700

    fix issue related connection db

--- StaffManangementService/Models/StaffDbContext.cs
+++ StaffManangementService/Models/StaffDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace StaffManagement.Service.Models;
 
@@ -19,7 +17,7 @@ public partial class StaffDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=CMTR-62001765;initial catalog=StaffManagement;persist security info=True;user id=sa;password=Adm1n@12345;MultipleActiveResultSets=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("name=StaffConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
